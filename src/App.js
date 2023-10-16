b652e4bd7fc62a9031c93b3a4ddba98565a2cf46
= <=>
2023-10-16 23:03:02 +0530

    The Initial code written without using toolchain manually configure all the structures of webpack and babel

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1>
+            Hello world! This Code is for Rtcamp Assignment for Reactjs
+        </h1>
+    )
+}
+
+export default App
